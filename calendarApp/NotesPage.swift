f8612695a0efcbaf010e8f0b4090dbdd42f65f77
Advik Arora <aroraadvik@gmail.com>
2024-04-02 12:34:23 -0400

    notes page

---
+++ calendarApp/NotesPage.swift
@@ -0,0 +1,18 @@
+//
+//  NotesPage.swift
+//  calendarApp
+//
+//  Created by Advik Arora on 4/2/24.
+//
+
+import SwiftUI
+
+struct NotesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesPage()
+}
