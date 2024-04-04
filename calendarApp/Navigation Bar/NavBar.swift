5cd093838c7b37e46c5186c98ce94af1e4ab7a98
Grace Odondi <godondi@unc.edu>
2024-04-04 18:47:16 -0400

    nav draft 1

---
+++ calendarApp/Navigation Bar/NavBar.swift
@@ -0,0 +1,48 @@
+//
+//  NavBar.swift
+//  calendarApp
+//
+//  Created by Grace Odondi on 4/4/24.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+    struct NavBar: View {
+        var body: some View {
+            NavigationView {
+                VStack {
+                    NavigationLink(destination: NotesPage()) {
+                        Text("Go to Notes Page")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                .navigationTitle("Home")
+            }
+        }
+    }
+
+    struct NotesPage: View {
+        var body: some View {
+            Text("This is the Notes Page")
+                .navigationTitle("Notes")
+        }
+    }
+
+    struct NavBar_Preview: PreviewProvider {
+        static var previews: some View {
+            NavBar()
+        }
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+}
