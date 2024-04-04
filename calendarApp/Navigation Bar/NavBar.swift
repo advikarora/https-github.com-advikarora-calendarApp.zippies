9bcfa3715364749fc72a768688f3ce3994c80a81
Grace Odondi <godondi@unc.edu>
2024-04-04 18:48:58 -0400

    nav draft 2

--- calendarApp/Navigation Bar/NavBar.swift
+++ calendarApp/Navigation Bar/NavBar.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-struct NavBar: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
     struct NavBar: View {
         var body: some View {
             NavigationView {
