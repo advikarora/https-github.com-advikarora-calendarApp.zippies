8b7b9d693a73ac83d4fc45f155131d106fc38dff
Grace Odondi <godondi@unc.edu>
2024-04-19 18:01:10 -0400

    AppNav (bottom navigation bar)

---
+++ calendarApp/Navigation Bar/SFSymbols.swift
@@ -0,0 +1,8 @@
+//
+//  SFSymbols.swift
+//  calendarApp
+//
+//  Created by Grace Odondi on 4/19/24.
+//
+
+import Foundation
---
+++ calendarApp/Navigation Bar/TabBarButton.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarButton.swift
+//  calendarApp
+//
+//  Created by Grace Odondi on 4/19/24.
+//
+
+import SwiftUI
+
+struct TabBarButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarButton()
+}
