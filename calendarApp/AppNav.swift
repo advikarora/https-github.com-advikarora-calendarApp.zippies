5fccbc64780ada84bddc638b782fa9092e1583a0
Grace Odondi <godondi@unc.edu>
2024-04-20 22:49:28 -0400

    AppNav draft 2.2

---
+++ calendarApp/AppNav.swift
@@ -0,0 +1,55 @@
+//
+//  AppNav.swift
+//  calendarApp
+//
+//  Created by Grace Odondi on 4/15/24.
+//
+
+import SwiftUI
+
+enum Tabs: Int {
+    case calendar = 0
+    case notes = 1
+}
+
+struct CustomTabBar: View {
+    
+    @Binding var selectedTab: Tabs
+    
+    var body: some View {
+        NavigationView {
+            HStack {
+                NavigationLink(destination: CalendarView()) {
+                    TabBarButton(buttonText: "Calendar", imageName: "calendar", isActive: selectedTab == .calendar)
+                }
+                .foregroundColor(.blue)
+                
+                Button(action: {
+                    // Home
+                }, label: {
+                    VStack (alignment: .center, spacing: 4) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 32, height: 32)
+                        
+                        Text("New Event")
+                    }
+                })
+                .foregroundColor(.blue)
+                
+                NavigationLink(destination: NotesPage()) {
+                    TabBarButton(buttonText: "Notes", imageName: "note.text", isActive: selectedTab == .notes)
+                }
+                .foregroundColor(.blue)
+            }
+            .frame(height: 82)
+        }
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBar(selectedTab: .constant(.calendar))
+    }
+}
