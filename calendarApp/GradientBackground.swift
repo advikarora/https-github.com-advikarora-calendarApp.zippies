f5fc8448499cc74ad5138f9024ac1b5f9b174c3f
Advik Arora <aroraadvik@gmail.com>
2024-04-25 15:14:38 -0400

    finalized app

---
+++ calendarApp/GradientBackground.swift
@@ -0,0 +1,18 @@
+//
+//  GradientBackground.swift
+//  calendarApp
+//
+//  Created by Advik Arora on 4/25/24.
+//
+
+import SwiftUI
+
+struct GradientBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientBackground()
+}
